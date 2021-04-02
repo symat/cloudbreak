17054c116d00470587f28173e56902b258d7f37b
Bryce Derriso <b.derriso@gmail.com>
2021-04-02 15:01:28 -0500

    Log AZ NIC public IP addresses when retrieved.

--- cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/AzureVmPublicIpProvider.java
+++ cloud-azure/src/main/java/com/sequenceiq/cloudbreak/cloud/azure/AzureVmPublicIpProvider.java
@@ -1,5 +1,7 @@
 package com.sequenceiq.cloudbreak.cloud.azure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.microsoft.azure.management.network.NetworkInterface;
@@ -8,12 +10,16 @@ import com.microsoft.azure.management.network.PublicIPAddress;
 @Component
 class AzureVmPublicIpProvider {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureVmPublicIpProvider.class);
+
     String getPublicIp(NetworkInterface networkInterface) {
         PublicIPAddress publicIpAddress = networkInterface.primaryIPConfiguration().getPublicIPAddress();
 
         if (publicIpAddress != null && publicIpAddress.ipAddress() != null) {
+            LOGGER.info("Azure network interface {} has public IP address {}.", networkInterface.id(), publicIpAddress.ipAddress());
             return publicIpAddress.ipAddress();
         } else {
+            LOGGER.info("Azure network interface {} does not have a public IP.", networkInterface.id());
             return null;
         }
     }
